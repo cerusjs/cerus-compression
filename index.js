ca97bdc5facb65f5b20731873ac65d3ce7032bda
JortvD <jortvandriel@hotmail.com>
2017-11-03 13:17:50 +0100

    Moved the settings to the init function

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ module.exports = function() {
 
 	self.init_ = function(cerus_) {
 		cerus = cerus_;
+
+		cerus.settings().setting("compression.on", true);
+		cerus.settings().setting("compression.chunk", zlib.Z_DEFAULT_CHUNK);
+		cerus.settings().setting("compression.level", zlib.Z_DEFAULT_COMPRESSION);
+		cerus.settings().setting("compression.memory", zlib.Z_DEFAULT_MEMLEVEL);
+		cerus.settings().setting("compression.strategy", zlib.Z_DEFAULT_STRATEGY);
+		cerus.settings().setting("compression.flush", zlib.Z_NO_FLUSH);
+		cerus.settings().setting("compression.finish", zlib.Z_FINISH);
+		cerus.settings().setting("compression.threshold", 1000);
+		cerus.settings().setting("compression.window", zlib.Z_DEFAULT_WINDOWBITS);
+		cerus.settings().setting("compression.type", "gzip");
 	}
 
 	self.compression = function(type) {

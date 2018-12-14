19a25c8ab880c18cd22c0a88ae887f8b5844caa0
JortvD <jortvandriel@hotmail.com>
2018-12-14 20:08:00 +0100

    Add a few classes to start testing

--- tests/compression.test.js
+++ tests/compression.test.js
@@ -1,5 +1,22 @@
-var expect = require("chai").expect;
+const expect = require("chai").expect;
+const cerus = require("cerus")();
+const compression_ = require("../lib/compression.js");
 
-describe("compression", function() {
+const compression = (type) => {
+	return new compression_(cerus, type); 
+}
 
+describe("compression", () => {
+	describe("#compress", () => {
+		context("with the data 'test 123 test'", () => {
+			it("should compress the data", done => {
+				compression().compress("test 123 test")
+				.then(data => {
+					console.log(data.toString());
+					done();
+				})
+				.catch(err => console.error(err));
+			});
+		});
+	});
 });
